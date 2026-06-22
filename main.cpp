84d894a2587588360c59b769f52d0d2b711c2166
Muhammad Nauman <maliknomigep@gmail.com>
2026-06-22 01:26:25 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start,end;
+    cout<<"enter your start range";
+    cin>>start;
+    cout<<"Enter your end range";
+    cin>>end;
+    int target_sum;
+    cout<<"enter your target sum";
+    cin>>target_sum;
+
+    for(int i=start;i<=end;i++)
+    {
+        for(int j=i+1;j<=end;j++)
+        {
+            if(j+i==target_sum)
+            {
+                cout << "(" << i << ", " << j << ")" << endl;
+            }
+        }
+    }
+    return 0;
+}
